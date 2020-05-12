36dc32f9cc109a2f74875bc213e0bddc8c56fbd6
yanivomc <yanivomc@gmail.com>
2020-05-12 23:13:23 +0300

    new php demo app deployed

--- app/models/micropost.rb
+++
@@ -1,4 +0,0 @@
-class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
-  validates :content, :length => { :maximum => 140 }
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'a06f81cf7657bb285452b36c68816e9861041998a312d1d920cc7d46e0820e55b7d7a837094eb46a0e835f8c96aee46494806df68bfddecafd4c0c2cbf98c8b3'
